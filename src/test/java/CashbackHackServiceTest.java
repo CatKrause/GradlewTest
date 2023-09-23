8b96d4048a77bdd4b29a6a42dc24aad4797e22c0
Catherine <catybym@gmail.com>
2023-09-23 18:29:36 +0300

    create tests junit4

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,7 +1,7 @@
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
+import org.junit.Test;
+import org.junit.Assert;
 import ru.netology.service.CashbackHackService;
+
 
 
 public class CashbackHackServiceTest {
